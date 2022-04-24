620f11cdb63962b0639537352f3cf0af11c69f64
trsimanto <simanto14feb@gmail.com>
2022-04-24 02:35:29 -0400

    done

--- Collapsable Toolbar SwiftUI/View/HomeView.swift
+++ Collapsable Toolbar SwiftUI/View/HomeView.swift
@@ -20,14 +20,14 @@ struct HomeView: View {
             VStack( spacing: 15){
                 GeometryReader{ proxy in
                     
-                    TopBar(topEdge: topEdge , offset: $offset)
+                    TopBar(topEdge: topEdge , offset: $offset , maxHeight : maxHeight )
                         .foregroundColor(.white)
                         .frame(maxWidth: .infinity)
                     //Sticky Effect ...
-                        .frame(height: maxHeight + offset , alignment: .bottom)
+                        .frame(height: getHeaderHeight() , alignment: .bottom)
                         .background(
                             Color("TopBar"),
-                            in: CustomCorner(corners: [.bottomRight], radius: 50)
+                            in: CustomCorner(corners: [.bottomRight], radius: getCornerRadious())
                         ).overlay(
                             // top Nav view
                             HStack( spacing: 15){
@@ -36,6 +36,19 @@ struct HomeView: View {
                                         .font(.body.bold())
                                     
                                 }
+                                
+                                Image("pic")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 35, height: 35)
+                                    .clipShape(Circle())
+                                    .opacity(topBartitleOpacity())
+                                
+                                Text("Pet Cat")
+                                    .bold()
+                                    .foregroundColor(.white)
+                                    .opacity(topBartitleOpacity())
+                                
                                 Spacer()
                                 Button(action: {}){
                                     Image(systemName: "line.3.horizontal.decrease")
@@ -44,8 +57,9 @@ struct HomeView: View {
                                 }
                             }.padding(.horizontal)
                             //maxheight
-                                .frame( height: 80 + topEdge )
+                                .frame( height: 80 )
                                 .foregroundColor(.white)
+                                .padding(.top , topEdge)
                             
                             
                             , alignment: .top
@@ -55,6 +69,7 @@ struct HomeView: View {
                 }.frame(height : maxHeight)
                 //fixing at top...
                     .offset(y: -offset )
+                    .zIndex(1)
                 //Sample Message
                 VStack(spacing: 15){
                     ForEach(allMessages){ message in
@@ -62,12 +77,41 @@ struct HomeView: View {
                         MessageCardView(message: message)
                     }
                 }.padding()
+                    .zIndex(0)
                 
             }.modifier(OffsetModifier(offset: $offset))
         }
         //setting coordinate space ..
         .coordinateSpace(name: "SCROLL")
     }
+    
+    func getHeaderHeight() -> CGFloat{
+        let topHeight =  maxHeight + offset
+        
+    // 80 is the constant top Nav bar height..
+        // sence we included top safe area so we also need to include that too ...
+        return topHeight > (80 + topEdge) ? topHeight : (80 + topEdge )
+        
+    }
+    
+    func getCornerRadious() -> CGFloat {
+        let progress = -offset / ( maxHeight - (80 + topEdge))
+        let value = 1 - progress
+        let radius = value * 50
+        return offset < 0 ? radius : 50
+    }
+    
+    func topBartitleOpacity() -> CGFloat {
+        //to start after the main content valished
+        // we need to eliminate 70 from offset ...
+        //to get starter
+        let progress = -(offset + 60) / (maxHeight - (80 + topEdge))
+      
+        
+        return progress
+        
+    }
+    
 }
 
 struct HomeView_Previews: PreviewProvider {
@@ -79,7 +123,7 @@ struct HomeView_Previews: PreviewProvider {
 struct TopBar : View {
     var topEdge : CGFloat
     @Binding var offset : CGFloat
-    
+    var maxHeight : CGFloat
     var body : some View {
         VStack(alignment: .leading, spacing: 15){
             Image("pic")
@@ -96,7 +140,19 @@ struct TopBar : View {
                 .opacity(0.8)
         }.padding()
             .padding(.bottom)
+            .opacity(getOpacity())
     }
+    
+    //Calculateion Opecity..
+    func getOpacity() -> CGFloat {
+        //70 = some random amount of time to visible on scroll
+        let progress = -offset / 70
+        let opacity = 1 - progress
+        
+        return offset < 0 ? opacity : 1
+    }
+    
+    
 }
 
 struct MessageCardView : View {
